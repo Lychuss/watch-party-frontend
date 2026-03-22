86b03fffa4d7d4985f744b5b0526eed224f26d19
Raphae San Juan <raphaelsanjuan6@gmail.com>
2026-03-23 00:01:21 +0800

    create responsive for xl size in frontapge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,38 +8,44 @@ import Cards from "@/components/Cards";
 
 export default function Home(){
   return <>
-  <div className="bg-[rgba(203,148,177,0.8)] flex flex-col items-center justify-center w-screen h-[400px]
-  border-b-2 border-black">
-    <img src="/icons/welcome-icon.svg" alt="welcome" className="w-[250px] z-1 mt-10
-    opacity-0 animate-[fadeUp_0.8s_ease-out_forwards]"></img>
-    <img src="/icons/paper-plane.svg" alt="thunder" className="w-[80px] absolute top-20 left-2 z-1
-    opacity-0 animate-[fadeUp_1s_ease-out_forwards]"></img>
-    <img src="/icons/flash.svg" alt="flash" className="w-[80px] absolute top-15 right-0 z-1
-    opacity-0 animate-[fadeUp_1.2s_ease-out_forwards]"></img>
-    <Labels label="Stream Watch Together" bg="bg-[rgba(247,74,163,0.8)] opacity-0 
-    animate-[fadeUp_1.5s_ease-out_forwards]"></Labels>
-  </div>
-  <div className="flex flex-col p-2 bg-[rgba(249,199,79,1)] gap-5 border-b-2 border-black">
-    <h1 className="text-5xl font-extrabold mt-3 opacity-0 
-    animate-[fadeUp_1.5s_ease-out_forwards]">Where Friends Meet Online</h1>
-    <Labels label="Step into a virtual space where you and your friends can watch movies, 
-    listen to music, video call, share reactions, chat in real-time, 
-    and create unforgettable moments together, no matter where you are in 
-    the world." bg="bg-[rgba(168,218,220,1)] tracking-wide opacity-0 
-    animate-[fadeUp_1.5s_ease-out_forwards]"></Labels>
-    <div className="flex items-center justify-center gap-20">
-      <img src="/icons/star.svg" className="w-[30px] opacity-0 
-    animate-[fadeUp_1.5s_ease-out_forwards]"></img>
-      <img src="/icons/star.svg" className="w-[30px] opacity-0 
-    animate-[fadeUp_1.5s_ease-out_forwards]"></img>
-      <img src="/icons/star.svg" className="w-[30px] opacity-0 
-    animate-[fadeUp_1.5s_ease-out_forwards]"></img>
+  <div className="xl:flex">
+    <div className="bg-[rgba(203,148,177,0.8)] flex flex-col items-center justify-center w-screen h-[400px]
+    border-b-2 border-black xl:h-screen xl:p-20">
+      <img src="/icons/welcome-icon.svg" alt="welcome" className="w-[250px] z-1 mt-10
+      opacity-0 animate-[fadeUp_0.8s_ease-out_forwards] xl:w-[350px] xl:mt-20"></img>
+      <img src="/icons/paper-plane.svg" alt="thunder" className="w-[80px] absolute top-20 left-2 z-1
+      opacity-0 animate-[fadeUp_1s_ease-out_forwards] xl:top-35 xl:w-[100px]"></img>
+      <img src="/icons/flash.svg" alt="flash" className="w-[80px] absolute top-15 right-0 z-1
+      opacity-0 animate-[fadeUp_1.2s_ease-out_forwards] xl:top-45 xl:w-[150px] xl:left-[31%]"></img>
+      <Labels label="Stream Watch Together" bg="bg-[rgba(247,74,163,0.8)] opacity-0 xl:w-[320px] 
+      xl:text-2xl animate-[fadeUp_1.5s_ease-out_forwards] text-center"></Labels>
     </div>
-    <div className="flex items-center justify-center mb-5">
-      <Button label="Read More" background="bg-[rgba(241,196,15,1)] max-w-[150px] rounded-l-full"
-       onClick={() => console.log()}></Button>
-      <Button label="Get Room" background="bg-[rgba(243,114,44,1)] max-w-[150px] rounded-r-full" 
-      onClick={() => console.log()}></Button>
+    <div className="flex flex-col p-2 bg-[rgba(249,199,79,1)] gap-5 border-b-2 border-black xl:h-screen xl:p-25">
+      <h1 className="text-5xl font-extrabold mt-3 opacity-0 
+        animate-[fadeUp_1.5s_ease-out_forwards] xl:mt-20 xl:text-6xl">Where Friends <br></br>Meet Online
+      </h1>
+      <div className="xl:flex xl:gap-10">
+        <Labels label="Step into a virtual space where you and your friends can watch movies, 
+          listen to music, video call, share reactions, chat in real-time, 
+          and create unforgettable moments together, no matter where you are in 
+          the world." bg="bg-[rgba(168,218,220,1)] tracking-wide opacity-0 
+          animate-[fadeUp_1.5s_ease-out_forwards] xl:text-lg xl:p-7"> 
+        </Labels>
+        <div className="flex items-center justify-center gap-20 xl:flex-col xl:gap-15 xl:w-[100px]">
+          <img src="/icons/star.svg" className="w-[30px] opacity-0 
+          animate-[fadeUp_1.5s_ease-out_forwards]"></img>
+          <img src="/icons/star.svg" className="w-[30px] opacity-0 
+          animate-[fadeUp_1.5s_ease-out_forwards]"></img>
+          <img src="/icons/star.svg" className="w-[30px] opacity-0 
+          animate-[fadeUp_1.5s_ease-out_forwards]"></img>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mb-5">
+        <Button label="Read More" background="bg-[rgba(241,196,15,1)] max-w-[150px] rounded-l-full"
+          onClick={() => console.log()}></Button>
+        <Button label="Get Room" background="bg-[rgba(243,114,44,1)] max-w-[150px] rounded-r-full" 
+          onClick={() => console.log()}></Button>
+      </div>
     </div>
   </div>
   <div className="bg-[rgba(215,217,215,1)] p-4 flex flex-col items-center justify-center gap-3">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,20 @@ import Dropdown from "./Dropdown";
 export default function Navbar(){
     const [dropDown, setDropdown] = useState<boolean>(false);
     return <>
-    <nav className="flex fixed justify-between w-full p-3 border-b-2 border-black bg-violet-400 z-100">
-        <div>
-            <h1 className="text-xl text-black font-extrabold">WatchParty</h1>
+    <nav className="flex fixed justify-between w-full p-3 border-b-2 border-black bg-violet-400 z-100 xl:p-8">
+        <div className="flex items-center justify-center">
+            <h1 className="text-xl text-black font-extrabold xl:text-3xl xl:mr-75">WatchParty</h1>
+            <ul className="hidden xl:flex gap-20 text-md font-bold self-center mr-80">
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>ROOM</li>
+            </ul>
+            <button className="hidden xl:flex gap-3 border-2 border-black bg-[rgba(241,196,15,1)] rounded-full px-5 py-3 font-semibold 
+        text-black shadow-[4px_4px_0_0] hover:bg-yellow-300 focus:ring-2 opacity-0 
+        animate-[fadeUp_1.5s_ease-out_forwards] focus:ring-yellow-300 focus:outline-0 w-full text-sm">
+            USER ID
+            <img src="/icons/user.svg" alt="user" className="w-[20px] h-auto"></img>
+        </button>
         </div>
         <div className="lg:hidden">
             <img src="/icons/menus.svg" alt="menus" className={`w-full max-w-[25px] transition-all duration-300
